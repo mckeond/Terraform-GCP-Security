63cf93e8a3b52b400a489d7572a89c5ae056231c
Dennis McKeon <93330480+mckeond@users.noreply.github.com>
2022-08-09 08:55:36 -0500

    Update outputs.tf

--- modules/VPC/outputs.tf
+++ modules/VPC/outputs.tf
@@ -1,44 +1,24 @@
-output "project" {
-  value = var.project_id
+output "network" {
+  value       = google_compute_network.network
+  description = "The VPC resource being created"
 }
 
-output "regions" {
-  value = var.region_cidrs
+output "network_name" {
+  value       = google_compute_network.network.name
+  description = "The name of the VPC being created"
 }
 
-output "vpc" {
-  value = google_compute_network.vpc.name
+output "network_id" {
+  value       = google_compute_network.network.id
+  description = "The ID of the VPC being created"
 }
 
-output "vpc_self_link" {
-  value = google_compute_network.vpc.self_link
+output "network_self_link" {
+  value       = google_compute_network.network.self_link
+  description = "The URI of the VPC being created"
 }
 
-output "subnetwork_objects" {
-  description = "The subnetwork object"
-  value       = google_compute_subnetwork.vpc_subnetworks
-}
-
-output "subnet_names" {
-  description = "The subnet names (in a regional map)"
-  value       = {
-    for region,subnet in google_compute_subnetwork.vpc_subnetworks: region => subnet.name
-  }
-}
-
-output "ip_cidr_ranges" {
-  description = "A list of the CIDR ranges in the network"
-  value       = [for subnet in google_compute_subnetwork.vpc_subnetworks: subnet.ip_cidr_range]
-}
-
-output "vpc_connectors" {
-  description = "A map (by region) of the VPC connector names"
-  value       = {
-    for region,connector in google_vpc_access_connector.connector: region => connector.name
-  }
-}
-
-output "private_service_connection" {
-  description = "The private service connection for the VPC"
-  value       = google_service_networking_connection.private_service_connection
+output "project_id" {
+  value       = var.shared_vpc_host && length(google_compute_shared_vpc_host_project.shared_vpc_host) > 0 ? google_compute_shared_vpc_host_project.shared_vpc_host.*.project[0] : google_compute_network.network.project
+  description = "VPC project id"
 }
